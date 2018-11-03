70b450f3f5b08f4ad2ba3a3f294058f2ad0fc7a7
RidhamArora <ridham0001@hotmail.com>
2018-11-04 00:06:14 +0530

    Visualized the 3d data

--- simple_linear_without_library.py
+++ simple_linear_without_library.py
@@ -2,7 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
+from mpl_toolkits.mplot3d import Axes3D
 # Importing the dataset
 dataset = pd.read_csv('Salary_Data.csv')
 X = dataset.iloc[:,:-1].values
@@ -48,23 +48,27 @@ def gradient(x_train,y_train,theta):
     return grad
     
 ###Algorithm
-def gradientDescent(x_train,y_train,learning_rate=0.001):
+def gradientDescent(x_train,y_train,learning_rate=0.1):
     theta=np.ones((2,))
+    theta[0]=-2
+    theta[1]=1
     error_list=[]
     itr = 0
-    max_itr=10000
+    max_itr=1000
     e=1000
+    theta_list=[]
     while(itr<max_itr):
         grad=gradient(x_train,y_train,theta)
         e=error(x_train,y_train,theta)
         error_list.append(e)
+        theta_list.append((theta[0],theta[1]))
         theta[0]=theta[0]-grad[0]*learning_rate
         theta[1]==theta[1]-grad[1]*learning_rate
         itr+=1
     print(itr)    
-    return theta,error_list
+    return theta,error_list,theta_list
 
-final_theta,error_list=gradientDescent(x_train,y_train)
+final_theta,error_list,theta_list=gradientDescent(x_train,y_train)
 plt.plot(error_list)
 plt.show()
 plt.scatter(x_train,y_train,color='Blue')
@@ -78,3 +82,46 @@ y_pred=hypothesis(x_test,final_theta)
 Y_pred=y_pred*Y_train.std()+Y_train.mean()
 plt.plot(X_test,Y_pred,color='Orange')
 plt.show()
+
+
+#Visualizing Cost function in 3d
+T0=np.arange(-2,3,0.01)
+T1=np.arange(-2,3,0.01)
+
+T0,T1=np.meshgrid(T0,T1)
+
+J=np.zeros(T0.shape)
+
+m=T0.shape[0]
+n=T1.shape[1]
+
+for i in range(m):
+    for j in range(n):
+        J[i,j]=np.sum((y_train-T1[i,j]*x_train-T0[i,j])**2)
+
+print(J.shape)
+
+theta_list=np.array(theta_list)
+
+fig = plt.figure()
+axes=fig.gca(projection='3d')
+axes.plot_surface(T0,T1,J,cmap='rainbow',alpha=0.05)
+axes.scatter(theta_list[:,0],theta_list[:,1],error_list,color='black',marker='^')
+plt.show()
+
+fig = plt.figure()
+axes=fig.gca(projection='3d')
+axes.contour(T0,T1,J,cmap='rainbow',alpha=0.5)
+axes.scatter(theta_list[:,0],theta_list[:,1],error_list,color='red',marker='^')
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
